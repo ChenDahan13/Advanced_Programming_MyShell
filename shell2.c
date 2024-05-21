20284a7feb6d9c282bb80666df2f34175a3eeee5
chendahan13 <hendahan13@gmail.com>
2024-05-21 11:17:15 +0300

    now you can do !!

--- shell2.c
+++ shell2.c
@@ -7,8 +7,29 @@
 #include "unistd.h"
 #include <string.h>
 
+void initialize_history_commands(char history_commands[20][1024]) {
+    for (int i = 0; i < 20; i++) {
+        strcpy(history_commands[i], "");
+    }
+}
+
+// add a command to the history of commands limit 20 commands
+void add_to_history_commands(char *command, char history_commands[20][1024]) {
+    for (int i = 19; i > 0; i--) {
+        strcpy(history_commands[i], history_commands[i - 1]);
+    }
+    strcpy(history_commands[0], command);
+}
+
+// return the last command from the history of commands
+char *get_last_command(char history_commands[20][1024]) {
+    return history_commands[0];
+}
+
 int main() {
 char command[1024];
+char history_commands[20][1024];
+initialize_history_commands(history_commands);
 char *token;
 char *outfile;
 int i, fd, amper, redirect, retid, status;
@@ -17,7 +38,8 @@ char *argv[10];
 char prompt[1024] = "hello: ";
 
 while (1)
-{
+{   
+    // get the command from the user
     printf("%s", prompt);
     fgets(command, 1024, stdin);
     command[strlen(command) - 1] = '\0';
@@ -27,6 +49,22 @@ while (1)
         strcpy(prompt, command + 9);
         strcat(prompt, " ");
         continue;
+    }
+
+    // check for !! command
+    if (strcmp(command, "!!") == 0) {
+        
+        char* last_command = get_last_command(history_commands);
+        
+        // if no commands in history
+        if (strcmp(last_command, "") == 0) {
+            printf("No commands in history\n");
+            continue;
+        } else { // execute the last command
+            strcpy(command, last_command);
+        }
+    } else { 
+        add_to_history_commands(command, history_commands);
     }
 
     /* parse command line */
@@ -39,6 +77,18 @@ while (1)
         i++;
     }
     argv[i] = NULL;
+
+    // check for cd command 
+    if (strcmp(argv[0], "cd") == 0) {
+        if (argv[1] == NULL) {
+            printf("No directory argument\n");
+        } else {
+            if (chdir(argv[1]) != 0) {
+                perror("cd");
+            }
+        }
+        continue;
+    }
 
     /* Is command empty */
     if (argv[0] == NULL)
